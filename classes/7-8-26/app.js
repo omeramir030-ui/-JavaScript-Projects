f1c05d40b94fe71756e9830b180cb7a80a916b26
omeramir030-ui <omeramir030@gmail.com>
2026-08-03 10:47:14 -0700

    Delete classes directory

--- classes/7-8-26/app.js
+++
@@ -1,9 +0,0 @@
-
-  function message(name, age) {
-     console.log("Hello, " + name + "! You are " + age + " years old.");
-     function greet() {
-        console.log("Welcome to the program, " + name + "!");
-     }
-     greet();
- }
- message("Omer", 14);
